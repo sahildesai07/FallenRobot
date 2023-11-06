8670f3e8ae5822f7a09ccfcb532d2c93798e8bc1
Sahil Desai <130888169+sahildesai07@users.noreply.github.com>
2023-11-06 12:20:28 +0000

    Update app.py
    Signed-off-by: Sahil Desai  <130888169+sahildesai07@users.noreply.github.com>

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
 from flask import Flask
-app = Flask(__name__)
+from aiohttp import web
 
-@app.route('/')
+# Flask app
+app_flask = Flask(__name__)
+
+@app_flask.route('/')
 def hello_world():
     return 'GreyMatters'
 
-
+# Run Flask app
 if __name__ == "__main__":
-    app.run()
+    app_flask.run()
+
+# aiohttp app
+routes = web.RouteTableDef()
+
+@routes.get("/", allow_head=True)
+async def root_route_handler(request):
+    return web.json_response("CodeXBotz")
+
+# Run aiohttp app
+if __name__ == "__main__":
+    app_aiohttp = web.Application()
+    app_aiohttp.router.add_routes(routes)
+    web.run_app(app_aiohttp)
